64585b420dab7d8a60b7bbe59a6c11aa5c12870d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-07-26 20:26:36 -0400

    fixed issue with travis

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PROJECT_DIRECTORY=$(.travis/build-condition.sh "$TRAVIS_COMMIT_RANGE" "$PROJECT")
-
-[ -d "$PROJECT_DIRECTORY" ] && \
-	cd "$PROJECT_DIRECTORY" && \
-		npx yarn install && npx yarn build
+.travis/build-condition.sh "$TRAVIS_COMMIT_RANGE" "$PROJECT" && \
+	[ -d "$PROJECT" ] && \
+		cd "$PROJECT" && \
+			npx yarn install && npx yarn build
