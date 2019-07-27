7c21bb109f324712cce7cad9bdad7815b36f5ca6
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-07-26 20:57:14 -0400

    fixed travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -3,14 +3,19 @@
 set -x
 
 if [[ -z $1 ]]; then
-    echo "Commit range cannot be empty"
+    echo "\$TRAVIS_COMMIT_RANGE must be defined."
     exit 1
 fi
 
-if git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null; then
-	cd "$PROJECT" && {
-		npx yarn install
-		npx yarn build
-	}
+if [[ -z $2 ]]; then
+    echo "Skipping because \$PROJECT is not defined."
+    exit 0
 fi
+
+git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null
+
+cd "$PROJECT" && {
+	npx yarn install
+	npx yarn build
+}
 
