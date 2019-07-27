de59b807783e48b2b93ce85c3a1f2d0d03c0096b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-07-26 21:17:24 -0400

    fixed issue with build.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# Print commands and their arguments as they are executed.
+set -x
+
+# Make sure both $TRAVIS_COMMIT_RANGE & $PROJECT are defined
+# before proceeding.
 if [[ -z $1 ]]; then
-    echo "\$TRAVIS_COMMIT_RANGE must be defined."
+    echo "\$TRAVIS_COMMIT_RANGE must not be empty."
     exit 1
 fi
 
@@ -10,9 +15,12 @@ if [[ -z $2 ]]; then
     exit 0
 fi
 
+# Check if $PROJECT folder contains any differences compared to the
+# last commit.
 git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null && {
+	# If there are differences, then change directory to $PROJECT.
 	cd "$PROJECT" && {
-		npx yarn install
-		npx yarn build
+		npx yarn install # Install dependencies.
+		npx yarn build # Build the project.
 	}
 }
