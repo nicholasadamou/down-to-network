aa620552f8354b8a96633a3da67ef5c7f493c115
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-07-26 20:52:17 -0400

    fixed issue with travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,11 +7,6 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
-if [[ -z $2 ]]; then
-    echo "Change path cannot be empty"
-    exit 1
-fi
-
 if git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null; then
 	cd "$PROJECT" && {
 		npx yarn install
