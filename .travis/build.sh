d39684cdf1844d6d4186b5cc57583eca4c9957ad
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-07-26 20:49:56 -0400

    fixed issue with travis

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,9 +12,10 @@ if [[ -z $2 ]]; then
     exit 1
 fi
 
-git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null && {
+if git diff --name-only "$1" | sort -u | uniq | grep "$2" > /dev/null; then
 	cd "$PROJECT" && {
 		npx yarn install
 		npx yarn build
 	}
-}
+fi
+
