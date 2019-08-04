d40c6b34523b7b54c6575488b342a64903bcd94a
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-08-04 16:22:56 -0400

    changed account route

--- frontend/src/constants/routes.js
+++ frontend/src/constants/routes.js
@@ -2,5 +2,5 @@ export const LANDING = '/';
 export const SIGN_IN = '/signin';
 export const SIGN_UP = '/signup';
 export const DASHBOARD = '/dashboard';
-export const ACCOUNT = '/account';
+export const ACCOUNT = '/account/edit';
 export const PASSWORD_FORGET = '/pw-forget';
