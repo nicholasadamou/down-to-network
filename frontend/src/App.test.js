b4569fc77a879cd31d048833ada3302b6df9fbbf
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-05-27 08:04:42 -0400

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -4,8 +4,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import Firebase, { FirebaseContext } from './contexts/Firebase'
+
+const root = () => (
+  <FirebaseContext.Provider value={new Firebase()}>
+    <App />
+  </FirebaseContext.Provider>
+);
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(root(), div)
   ReactDOM.unmountComponentAtNode(div)
 })
